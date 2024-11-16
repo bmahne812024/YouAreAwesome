5527b574b28afa0121510bdcccc5d9d9571ccc0a
Brian Mahne <bmahne81@gmail.com>
2024-11-15 21:48:37 -0500

    Used functions and play sound

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,22 +6,33 @@
 //
 
 import SwiftUI
+import AVFAudio
+
 
 struct ContentView: View {
     @State private var message = ""
+
     @State private var imageName = ""
+    @State private var newImageName = ""
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var imageNum = 0
+    @State private var messageNum = 0
+    @State private var soundNum = 0
+    
     let messages = [
         "You are great!",
         "You are awesome!",
         "You are amazing!"
     ]
     
+
+    
     var body: some View {
         
         ZStack {
             LinearGradient(colors: [.purple, .white], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
-            
+
             VStack {
                 Spacer()
                 Image(imageName)
@@ -33,14 +44,49 @@ struct ContentView: View {
                 
                 Spacer()
                 Button("Push me") {
-                    message = messages[Int.random(in: 0...2)]
-                    imageName = "animal\(Int.random(in: 1...3))"
+                    print("\nOld messageNum = \(messageNum)")
+                    messageNum = nonRepeatingRandom(lastValue: messageNum, range: messages.count)
+                    print("New messageNum = \(messageNum)")
+                    message = messages[messageNum-1]
+                    
+                    print("\nOld imageNum = \(imageNum)")
+                    
+                    imageNum = nonRepeatingRandom(lastValue: imageNum, range: 3)
+                    imageName = "animal\(imageNum)"
+
+                    var soundName = ""
+                    soundNum = nonRepeatingRandom(lastValue: soundNum, range: 5)
+                    soundName = "Slot Machine Mega Win_\(soundNum)"
+                    playSound(soundName: soundName)
+                    
                 }
                 .buttonStyle(.borderedProminent)
             }
             .padding()
         }
     }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("error loading file")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("Error")
+        }
+    }
+    
+    func nonRepeatingRandom(lastValue: Int, range: Int) -> Int {
+        var newValue = 0
+        repeat {
+            newValue = Int.random(in: 1...range)
+        } while newValue == lastValue
+        return newValue
+    }
+    
 }
 
 #Preview {
